bea3bfcc31962b5449d816ca5ab731cf8298c38b
pvig <patricevigouroux@yahoo.fr>
2023-04-04 22:05:19 +0200

    compile error `vtable for SynthParamListener'

--- firmware/Src/synth/SynthParamListener.h
+++ firmware/Src/synth/SynthParamListener.h
@@ -27,7 +27,7 @@ class SynthParamListener {
 public:
     virtual void newParamValue(int timbre, int currentrow, int encoder, ParameterDisplay* param, float oldValue, float newValue) = 0;
     virtual void newParamValueFromExternal(int timbre, int currentrow, int encoder, ParameterDisplay* param, float oldValue, float newValue) = 0;
-    virtual void newMixerValueFromExternal(int timbre, int mixerValueType, float oldValue, float newValue);
+    virtual void newMixerValueFromExternal(int timbre, int mixerValueType, float oldValue, float newValue) = 0;
     virtual void newTimbre(int timbre) = 0;
     virtual void newMixerValue(uint8_t valueType, uint8_t timbre, float oldValue, float newValue) = 0;
     virtual void newMixerEdit(int oldButton, int newButton) = 0;
